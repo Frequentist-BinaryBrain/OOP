8d209cca827d398641e43d4557714524b86992d4
Frequentist-BinaryBrain <pkarampudi@arizona.edu>
2024-10-23 11:51:13 +0530

    DiceGame-part1

--- Dice_game/OOP.py
+++ Dice_game/OOP.py
@@ -43,6 +43,50 @@ class Player:
     def roll_die(self):
         return self._die.roll() # we will call this roll from die class
    
+
+class DiceGame():
+
+    def __init__(self, player, computer):
+        self._player = player
+        self._computer = computer
+    
+    def play(self):
+        print("================================")
+        print("🎲 Welcome to Roll the Dice! 🎲")
+        print("================================")
+        while True:
+            self.play_round()
+            # TODO: implement gameover
+
+    def play_round(self):
+        # Welcome the user
+        print("------- New Round -------")
+        input("Press any key to Roll the dice 🎲")
+
+        #Roll te dice 
+        player_value = self._player.roll_die()
+        computer_value = self._computer.roll_die()
+
+        #show the values
+        print(f'Your dice: {player_value}')
+        print(f'Computer dice: {computer_value}')
+
+        #Determine Winner and Loser
+        if player_value > computer_value:
+            print("You won the round") 
+            self._player.decrement_counter() # Winner
+            self._computer.increment_counter() # Loser
+        elif computer_value > player_value:
+            print("The computer won the round")
+            self._player.increment_counter() # Loser
+            self._computer.decrement_counter() # Winner
+        else:
+            print("it is a TIE!!")
+
+        print(f"Your counter: {self._player.counter}")
+        print(f"Computer counter: {self._computer.counter}")
+
+
 # Testing Die class 
 die_game = Die()
 
@@ -67,4 +111,17 @@ my_player.decrement_counter()
 print(my_player.counter)
 my_player.roll_die()
 print("Die value : ", my_die.value)
+
+#Testing DIceGmae class
+print("Testing the DiceGame class")
+player_die = Die()
+computer_die = Die()
+
+my_player = Player(player_die, is_computer=False)
+computer_player = Player(computer_die, is_computer=True)
+
+game = DiceGame(my_player, computer_player)
+
+#Start the game
+game.play()
 
